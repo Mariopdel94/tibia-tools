1de2a833df7416609a4fc9febd2ce84200e15a0e
Mario Pineda <mpin3027@7-11.com>
2025-12-22 11:43:59 -0700

    feat: removed unnecessary code

--- src/app/components/live-session-instructions/live-session-instructions.ts
+++ src/app/components/live-session-instructions/live-session-instructions.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 
@@ -9,10 +9,4 @@ import { MatIconModule } from '@angular/material/icon';
   templateUrl: './live-session-instructions.html',
   styleUrl: './live-session-instructions.scss',
 })
-export class LiveSessionInstructions {
-  collapsed = signal(false);
-
-  toggle() {
-    this.collapsed.update((v) => !v);
-  }
-}
+export class LiveSessionInstructions {}
